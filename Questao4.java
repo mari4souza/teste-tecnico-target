17a12efcf53a1a09ff57d8d19925bec6cb691da9
mari4souza <meduardacardoso.121@gmail.com>
2024-03-12 12:17:30 -0300

    all done

--- Questao4.java
+++ Questao4.java
@@ -1,3 +1,20 @@
+// 4) Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em uma sala diferente. Você não pode ver as lâmpadas da sala 
+// em que está, mas pode ligar e desligar os interruptores quantas vezes quiser. Seu objetivo é descobrir qual interruptor controla qual lâmpada.
+// Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada? 
+import java.util.Scanner;
+
 public class Questao4 {
-    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+        // Inicar um array com os valores setados e 0 para simular os interruptores desligados
+
+    }
 }
+
+
