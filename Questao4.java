5cab4fcf89ab0984526ca393afd857a8aad0c8cc
mari4souza <meduardacardoso.121@gmail.com>
2024-03-12 23:50:40 -0300

    all done

--- Questao4.java
+++ Questao4.java
@@ -3,26 +3,31 @@
 // Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada? 
 
 /*
- * Acredito que há duas formas de se resolver este problema: 
+ * Resolvendo o problema: 
  * 
  * 1. Pensando em um caso mais realista, no qual consigo fisicamente sentir a temperatura das lâmpadas: 
- * -> Ligo um dos interruptores
+ * -> Ligo o interruptor A
  * -> Aguardo alguns minutos até que a lâmpada esquente
  * -> Desligo este primeiro interruptor
- * -> Ligo o segundo interruptor
+ * -> Ligo o segundo interruptor B
  * 
- *      Na primeira ida até a sala,
- *          - A lâmpada que está acesa é controlada pelo segundo interruptor
- *          - A lâmpada que está apagada, mas quente, é controlada pelo primeiro interruptor
- *          - A lâmpada que está apagada e fria, é controlada pelo terceiro interruptor
+ *      Na primeira ida até uma das salas,
+ *          - Se a lâmpada daquela sala está acesa, é controlada pelo B
+ *          - Se a lâmpada daquela sala está apagada, mas quente, é controlada pelo A
+ *          - Se a lâmpada daquela sala está apagada e fria, é controlada pelo terceiro interruptor, C
  * 
- *   Neste caso, eu conseguiria descobrir qual interruptor controla qual lâmpada com apenas uma visita até a sala onde estão as lâmpadas.
+ * Neste caso eu conseguiria descobrir qual interruptor controla uma das lâmpadas e eliminaria este da jogada, sendo assim,
+ * sobrariam dois outros interruptores.
+ * -> Ligo um deles, por exemplo o interruptor B
  * 
- *  1. Caso eu não tenha a possibilidade de verificar fisicamente, posso resolver com os seguintes passos:
- * -> Ligo um dos interruptores
- * -> Vou até a sala e verifico qual lâmpada este interruptor acende.
+ *      Na segunda ida até uma das salas, 
+ *          - Se a lampada daquela sala está acesa, ela é controlada pelo interruptor B e consequentemente, a lâmpada da outra sala 
+ *            é controlada pelo C.
+ *          - Se o cenário for contrário e a lâmpada estiver apagada, então ela é controlada pelo C e a da outra sala pelo B. 
+ * 
+ * Dessa forma, eu saberia qual interruptor controla qual lâmpada visitando apenas duas das salas. :)     
+ *         
  */
-
 
 
 import java.util.Scanner;
